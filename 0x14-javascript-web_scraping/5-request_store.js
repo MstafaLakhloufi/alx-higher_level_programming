17d85ef6d1da70ec2c1e651034a42790dba4286c
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2024-08-12 11:28:32 -0700

    m

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,8 +2,8 @@
 const fs = require('fs');
 const request = require('request');
 
-request(process.argv[2], function (err, response, body) {
-  if (err == null) {
-    fs.createWriteStream(process.argv[3], body);
-  }
-});
+if (process.argv.length > 3) {
+  request
+    .get(`${process.argv[2]}`)
+    .pipe(fs.createWriteStream(process.argv[3]));
+}
